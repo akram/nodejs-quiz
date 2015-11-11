cd3ad4360b3fbac9dfc305ae7d6b08e22e5681e9
Akram <akram@free.fr>
2015-11-11 11:52:35 +0100

    Initial commit

---
+++ config/questions.js
@@ -0,0 +1,159 @@
+// -- List of questions -- //
+// We'll see after to user MONGODB to store them
+module.exports = 
+[
+	{
+		text : "What is the capital of France ?",
+		propositions :
+		[
+			{
+				text : "Paris",
+				correct : true
+			},
+			{
+				text : "Berlin",
+				correct : false
+			},
+			{
+				text : "Moscow",
+				correct : false
+			},
+			{
+				text : "London",
+				correct : false
+			}
+		]
+	},
+	{
+		text : "Which coutries are part of the European Union",
+		propositions :
+		[
+			{
+				text : "Greece",
+				correct : true
+			},
+			{
+				text : "Ukraine",
+				correct : false
+			},
+			{
+				text : "Littuania",
+				correct : true
+			},
+			{
+				text : "Hungary",
+				correct : true
+			}
+		]
+	},
+	{
+		text : "In which movie Bruce Willis hasn't played",
+		propositions :
+		[
+			{
+				text : "Titanic",
+				correct : true
+			},
+			{
+				text : "Lord of the Rings",
+				correct : true
+			},
+			{
+				text : "Die Hard 3",
+				correct : false
+			},
+			{
+				text : "Armageddon",
+				correct : false
+			}
+		]
+	},
+	{
+		text : "Who is Kevin Mitnick",
+		propositions :
+		[
+			{
+				text : "A Hacker",
+				correct : true
+			},
+			{
+				text : "The president of the USA",
+				correct : false
+			},
+			{
+				text : "An Actor",
+				correct : false
+			},
+			{
+				text : "Your mother",
+				correct : false
+			}
+		]
+	},
+	{
+		text : "How many countries in EU",
+		propositions :
+		[
+			{
+				text : "12",
+				correct : false
+			},
+			{
+				text : "20",
+				correct : false
+			},
+			{
+				text : "28",
+				correct : true
+			},
+			{
+				text : "32",
+				correct : false
+			}
+		]
+	},
+	{
+		text : "Who created Facebook",
+		propositions :
+		[
+			{
+				text : "Bill Gates",
+				correct : false
+			},
+			{
+				text : "Me",
+				correct : false
+			},
+			{
+				text : "Mark Zuckerberg",
+				correct : true
+			},
+			{
+				text : "Walt Disney",
+				correct : false
+			}
+		]
+	},
+	{
+		text : "What is the name of Howarts Director",
+		propositions :
+		[
+			{
+				text : "Albus Dumbledore",
+				correct : true
+			},
+			{
+				text : "Severus Snape",
+				correct : false
+			},
+			{
+				text : "Harry Potter",
+				correct : false
+			},
+			{
+				text : "Tom Riddle",
+				correct : false
+			}
+		]
+	}
+]
